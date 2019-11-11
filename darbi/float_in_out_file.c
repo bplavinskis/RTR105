e568e9e9270419a8498e19061bd120d846c225b0
bplavinskis <54942913+bplavinskis@users.noreply.github.com>
2019-11-11 23:59:44 +0200

    Update float_in_out_file.c

--- darbi/float_in_out_file.c
+++ darbi/float_in_out_file.c
@@ -5,22 +5,24 @@ int main(){
 	float s1[5][2];
 	float s2[5][3];
 	int i,j;
-	printf("Ievadiet 10 reālus skaitļus: \n");
+	printf("Ievadiet 10 re?lus skait?us: \n");
 	for(i=0,i<5,i++)
 	{
 		for(j=0,j<2,j++)
 		{
-`			printf("Ievadiet skaitli [%d][%d] laukā: ",i,j);
-			scanf("%d", &s1[i][j]);
+			printf("Ievadiet skaitli [%d]. rind? [%d]. kolonn?: ",i+1,j+1);
+			scanf("%lf", &s1[i][j]);
 		}
 	}
 
 	f = fopen("float_in_out_file.txt","w+");
 	for(i=0,i<5,i++)
 	{
-		for(j=0,j<2,j++){
-`			fprintf(f,"%d ",s1[i][j]);
+		for(j=0,j<2,j++)
+		{
+			fprintf(f,"%f ",s1[i][j]);
 		}
+		fprint{f,"\n"}
 	}
 	fclose (f);
 
@@ -29,24 +31,10 @@ int main(){
 	{
 		for(j=0,j<2,j++)
 		{
-			fscanf(f,"%d", &s2[i][j]);
+			fscanf(f,"%lf", &s2[i][j]);
 		}
-		s2[i][3] = s2[i][1] * s2[i][2]
+	printf("%f %f %f \n",s2[i][0],s2[i][1],s2[i][0] * s2[i][1]);
 	}
-
-	printf("Rezultāts:");
-	for(i=0; i<5; i++)
-	{
-		for(j=0;j<3;j++)
-		{
-		printf("%d ", disp[i][j]);
-		if(j==3)
-		{
-			printf("\n");
-		}
-      	}
-   }
-
 	fclose (f);
 
 }
