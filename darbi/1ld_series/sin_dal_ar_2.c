60b4e1ce3874f01963adf7941417a306598757ee
bplavinskis <54942913+bplavinskis@users.noreply.github.com>
2020-01-18 12:25:45 +0200

    Update and rename sin_dal_ar_2_caur_rindu.c to sin_dal_ar_2.c

---
+++ darbi/1ld_series/sin_dal_ar_2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+ long double x,y,a,s;
+ int k=0;
+
+ printf("Ievadiet x vērtību: ");
+ scanf("%Lf",&x);
+ y = sin(x/2);
+ printf("y=sin(%.2Lf/2)=%.2Lf\n",x,y);
+
+ a = pow(-1,k)*pow(x,2*k+1)/(/*((2*k+1)!)*/*pow(2,2*k+1));
+ s += a;
+
+ while (k<500)
+ {
+  if (k == 0)
+   printf("a0 = %Le\tS0 =%.2Lf\n",a,s);
+  k++;
+  a = a*x/(k);
+  s += a;
+  if (k == 499)
+   printf("a499 = %Le\tS499 =%.2Lf\n",a,s);
+ }
+ printf("a500 = %Le\tS500 =%.2Lf\n",a,s);
+
+ printf("\n");
+ printf("\t\t 500\n");
+ printf("\t\t----\n");
+ printf("\t\t\\\t\t    k    2k+1\n");
+ printf("\t\t \\\t\t(-1) * x\n");
+ printf("f(x)=\t\t  |\t  ----------------\n");
+ printf("\t\t /\t\t        2k+1\n");
+ printf("\t\t/\t\t  (2k+1)!*2\n");
+ printf("\t\t----\n");
+ printf("\t\tk=0\n");
+
+ printf("\n");
+ printf("\tx\n");
+ printf("R= --------\n");
+ printf("\tk\n");
+}
