33cda16474cc1643e08bb8de3b8ba45df57657b0
bplavinskis <54942913+bplavinskis@users.noreply.github.com>
2020-01-18 12:54:49 +0200

    Update 2ld.c

--- darbi/2ld_roots/2ld.c
+++ darbi/2ld_roots/2ld.c
@@ -1,29 +1,36 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
- float a=0.01, b=1.5*M_PI, x, delta_x=1.e-5/*0.001*/, funkca, funkcb, funkcx;
+ float a, b, c, x, delta_x, funkca, funkcb, funkcx;
  int k=0;
- funkca = sin(a/2); funkcb = sin(b/2);
- if(funkca*funkcb>0){
-  printf("Intervālā [%.2f;%.2f] sin(x/2) funkcijai ",a,b);
-  printf("sakņu nav (vai tājā ir pāru sakņu skaits)\n");
-  return 1;}
+  
+  printf("Ievadiet sākuma robežu (a): ");
+  scanf ("%f", &a);
+  printf("Ievadiet beigas robežu (b): ");
+  scanf ("%f", &b);
+  printf("Ievadiet precizitāti: ");
+  scanf ("%e", &delta_x);
+  printf("Ievadiet, kādam y Jūs gribat aprēķināt x: ");
+  scanf ("%f", &c);
 
- printf("               sin(%7.3f/2)=%7.3f\t\t",a,sin(a/2));
- printf("sin(%7.3f/2)=%7.3f\n",b,sin(b));
-
- while((b-a)>delta_x){
-  k++;//k=k+1;//k+=1;
+ funkca = sin(a/2)-c; funkcb = sin(b/2)-c;
+ 
+ printf("sin(%7.3f/2) - %7.3f =%7.3f\t\t\t\t\t\t\t",a,c,funkca);
+ printf("sin(%7.3f/2) - %7.3f =%7.3f\n",b,c,funkcb);
+ 
+ while ((b-a)>delta_x){
   x = (a+b)/2.;
-  if(funkca*sin(x/2)>0) // pie a=0 -> funkca=0 -> reizinājums ir precīzi 0 visu laiku -> visu laika "strādā" b=x
+  if(funkca*(sin(x/2)-c)>0)
    a = x;
   else
    b = x;
-  printf("%2d. iterācija: sin(%7.3f/2)=%7.3f\t",k,a,sin(a/2));
-  printf("sin(%7.3f/2)=%7.3f\t",x,sin(x/2));
-  printf("sin(%7.3f/2)=%7.3f\n",b,sin(b/2));}
+  printf("sin(%7.3f/2) - %7.3f =%7.3f\t",a,c,funkca);
+  printf("sin(%7.3f/2) - %7.3f =%7.3f\t",x,c,funkcb);
+  printf("sin(%7.3f/2) - %7.3f =%7.3f\n",b,c,sin(x/2)-c);
+   i++;
+ }
 
- printf("Sākne atrodas pie x=%.3f, jo sin(x/2) ir %.3f\n",x,sin(x/2));
-
+ printf("\nIteraciju skaits: %d\n",k);
+ printf("Sākne atrodas pie x=%.5f, jo sin(x/2) ir %.3f\n",x,sin(x/2);
  return 0;
 }
