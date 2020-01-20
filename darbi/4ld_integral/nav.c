caa36b91ad4d752d65bd411bb0215b812a446bab
bplavinskis <54942913+bplavinskis@users.noreply.github.com>
2020-01-20 18:45:56 +0200

    Create nav.c

---
+++ darbi/4ld_integral/nav.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+ print("  _   _       __      __\n");
+ print(" | \ | |    /\\ \    / /\n");
+ print(" |  \| |   /  \\ \  / / \n");
+ print(" | . ` |  / /\ \\ \/ /  \n");
+ print(" | |\  | / ____ \\  /   \n");
+ print(" |_| \_|/_/    \_\\/    \n")
+ print("                        \n");
+}
